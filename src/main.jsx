f0b2d901deac2ca586572bff769a680c30093ebd
YunheeAhn <ayh95944674@gmail.com>
2025-11-07 19:51:12 +0900

    feat : 리액트쿼리 설정

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
