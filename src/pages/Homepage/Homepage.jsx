906317633e1a92c6ea3cbf07d879ec5c0d62a9f4
YunheeAhn <ayh95944674@gmail.com>
2025-11-07 20:06:02 +0900

    chore : 배너 컴포넌트 연결

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
+
+// 1. 배너 생성 => popular 영화의 첫번째 아이템
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
+// 5.
 
 const Homepage = () => {
-  return <div>홈페이지</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Homepage;
