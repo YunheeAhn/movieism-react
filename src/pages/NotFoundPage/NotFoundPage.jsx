5c94f1aae8a46f88e1630ea0747ec3da50718fac
YunheeAhn <ayh95944674@gmail.com>
2025-11-07 23:27:27 +0900

    style : 404 not fount이미지 추가

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>경로잘못들어가서아무것도없는페이지</div>;
+  return (
+    <div className="not-found-page">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTksk08zE0-KIhNxAoRgEVFqVo4YlivwUGGqQ&s"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default NotFoundPage;
