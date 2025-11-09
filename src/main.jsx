57b2a528df5e9bf2b2ea7768b725a169df8be132
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 23:40:21 +0900

    style : 슬릭 슬라이더 css 추가

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
