c960519b83ee0bcbeaeabc7db297f1c8fb3cf0c7
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 16:52:57 +0900

    feat : PopularMovieSlide.jsx추가

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 
 // 1. 배너 생성 => popular 영화의 첫번째 아이템
 // 2. popular movie
@@ -11,6 +12,9 @@ const Homepage = () => {
   return (
     <div>
       <Banner />
+      <section className="w1560">
+        <PopularMovieSlide />
+      </section>
     </div>
   );
 };
