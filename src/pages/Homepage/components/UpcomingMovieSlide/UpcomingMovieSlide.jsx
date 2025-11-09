224b152e3fa577d4cfe89682e781e7a4dd29516f
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 23:51:55 +0900

    feat : 슬라이더 브레이크포인트 변경

--- src/pages/Homepage/components/UpcomingMovieSlide/UpcomingMovieSlide.jsx
+++ src/pages/Homepage/components/UpcomingMovieSlide/UpcomingMovieSlide.jsx
@@ -29,20 +29,8 @@ const UpcomingMovieSlide = () => {
     slidesToShow: 6,
     slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 981,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 681,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
+      { breakpoint: 1024, settings: { slidesToShow: 3 } },
+      { breakpoint: 768, settings: { slidesToShow: 2 } },
     ],
   };
   return (
