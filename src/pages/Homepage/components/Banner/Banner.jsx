a3668e8efc081fcb4a2558348aa95e79b110c7b4
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 18:55:24 +0900

    refactor : 배너 슬라이드 추가 및 코드 리팩토링

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -2,34 +2,50 @@ import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 import "./Banner.style.css";
 
+import Slider from "react-slick";
+
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log(data);
 
   if (isLoading) {
-    <h1 className="message">Loading...</h1>;
+    return <h1 className="message">Loading...</h1>;
   }
 
   if (isError) {
-    <h1 className="message">{error.message}</h1>;
+    return <h1 className="message">{error.message}</h1>;
   }
 
+  const bannerImages = data.results.slice(0, 3).map((movie) => ({
+    img: `https://media.themoviedb.org/t/p/w533_and_h300_bestv2${movie.poster_path}`,
+    title: movie.title,
+    overview: movie.overview,
+  }));
+
+  const settings = {
+    dots: false,
+    autoplay: true, // 자동 재생 활성화
+    autoplaySpeed: 5000,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <div className="banner">
-      <div
-        className="banner-img"
-        style={{
-          backgroundImage:
-            "url(" +
-            `https://media.themoviedb.org/t/p/w533_and_h300_bestv2${data?.results[0].poster_path}` +
-            ")",
-        }}
-      ></div>
-      <div className="banner-text">
-        <h2>{data?.results[0].title}</h2>
-        <p>{data?.results[0].overview}</p>
-      </div>
-    </div>
+    <Slider {...settings} className="visual">
+      {bannerImages.map((movie, index) => (
+        <dl className="banner" key={index}>
+          <dt className="banner-img">
+            <img src={movie.img} alt={movie.title} />
+          </dt>
+          <dd className="banner-text">
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </dd>
+        </dl>
+      ))}
+    </Slider>
   );
 };
 
