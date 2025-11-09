32594362631ce9efc56eb8b5b68e0873b2fd890a
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 16:54:11 +0900

    feat : 헤더 스크롤 효과 추가

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 import logo from "../assets/logo.png";
 
 const AppLayout = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // 컴포넌트 언마운트 시 이벤트 제거
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="wrapper">
-      <div className="header">
+      <div className={`header ${isScrolled ? "scrolled" : ""}`}>
         <div className="logo">
           <img src={logo} alt="MOVIEISM" />
         </div>
