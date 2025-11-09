b0b74009fb016430ab597cc0671ebdd9aebbb81e
YunheeAhn <ayh95944674@gmail.com>
2025-11-09 23:48:58 +0900

    fix : slick-theme 버그 수정

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const queryClient = new QueryClient();
 
