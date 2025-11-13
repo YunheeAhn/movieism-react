0a97f811b306a75caecc4d51d8816b5ca15eac0d
YunheeAhn <ayh95944674@gmail.com>
2025-11-13 15:41:42 +0900

    feat : useParams 로 id 넘겨주기

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import DetailCard from "./component/DetailCard/DetailCard";
+import Review from "./component/Review/Review";
+import { useParams } from "react-router";
 
 const MovieDetail = () => {
+  const { id } = useParams();
+
   return (
     <section className="movies-detail w1700">
       <div className="detail-card">
-        <DetailCard />
+        <DetailCard id={id} />
       </div>
-      <div className="review"></div>
+      <div className="review">
+        <h2>Reviews</h2>
+        <Review id={id} />
+      </div>
       <div className="recommend"></div>
     </section>
   );
