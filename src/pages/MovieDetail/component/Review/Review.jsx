1bae81bf1bab71db5f236847e9a9c0bfd835ef53
YunheeAhn <ayh95944674@gmail.com>
2025-11-13 17:56:50 +0900

    feat : 리뷰 컨텐츠 open,close

--- src/pages/MovieDetail/component/Review/Review.jsx
+++ src/pages/MovieDetail/component/Review/Review.jsx
@@ -1,10 +1,11 @@
 // Review.jsx
-import React from "react";
+import React, { useState } from "react";
 import { useMovieReviewsQuery } from "../../../../hooks/useMovieReviews";
 
 const Review = ({ id }) => {
   const { data: response, isLoading, isError, error } = useMovieReviewsQuery(id);
 
+  const [openContent, SetOpenContent] = useState(null);
   if (isLoading) {
     return <h1 className="message">Loading...</h1>;
   }
@@ -15,6 +16,10 @@ const Review = ({ id }) => {
 
   const reviews = response?.data?.results ?? [];
 
+  const toggleContent = (index) => {
+    SetOpenContent(openContent === index ? null : index); // 같은 버튼 클릭 시 닫기
+  };
+
   return (
     <div className="review-wrap">
       {reviews.length === 0 ? (
@@ -23,7 +28,12 @@ const Review = ({ id }) => {
         reviews.map((review, index) => (
           <dl key={index} className="review-item">
             <dt className="author">{review.author}</dt>
-            <dd className="content">{review.content}</dd>
+            <dd className="content">
+              {openContent === index ? <p>{review.content}</p> : null}
+              <button onClick={() => toggleContent(index)}>
+                {openContent === index ? "Close Review" : "Show Review"}
+              </button>
+            </dd>
           </dl>
         ))
       )}
