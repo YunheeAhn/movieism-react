736f8da5c995098691e2f1237113b4fbb1f04e3b
YunheeAhn <ayh95944674@gmail.com>
2025-11-14 22:53:21 +0900

    chore : 리액트 멀티 캐러셀 반응형 아이템 수 수정

--- src/constants/responsive.js
+++ src/constants/responsive.js
@@ -1,15 +1,15 @@
 export const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 1500 },
-    items: 6,
+    items: 4,
   },
   desktop: {
     breakpoint: { max: 1500, min: 1024 },
-    items: 5,
+    items: 3,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
