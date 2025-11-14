3d18cc85df3f2dfbab55119868b54c0bd317a445
YunheeAhn <ayh95944674@gmail.com>
2025-11-14 22:47:53 +0900

    feat : responsive constants 생성

---
+++ src/constants/responsive.js
@@ -0,0 +1,18 @@
+export const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1500 },
+    items: 6,
+  },
+  desktop: {
+    breakpoint: { max: 1500, min: 1024 },
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
